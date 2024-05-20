2c8df1ade183295a9f30efd7e4a0f24a63ce647a
dev-coding-for-fun <dserink@gmail.com>
2024-05-20 12:42:14 -0600

    cleanup kysely FTS5 workaround with cleaner approach

--- app/routes/api.search.tsx
+++ app/routes/api.search.tsx
@@ -1,10 +1,9 @@
 import { LoaderFunction, json } from '@remix-run/cloudflare'; // assuming Remix is being used
-import { Kysely, ReferenceExpression, sql } from 'kysely';
+import { Kysely, sql } from 'kysely';
 import { D1Dialect } from 'kysely-d1';
 import { Env } from '~/lib/db';
 import { DB } from 'kysely-codegen';
-import { SearchRoutesResponse } from './_issues.createIssue';
-import { SelectExpression } from 'kysely';
+import { SearchRoutesResponse } from '../components/routeSearchBox';
 
 export const loader: LoaderFunction = async ({ request, context }) => {
     const url = new URL(request.url);
@@ -23,12 +22,9 @@ export const loader: LoaderFunction = async ({ request, context }) => {
         dialect: new D1Dialect({ database: DB }),
     });
 
-    //Kysely doesn't fully support FTS5 tables with proper typing, so these refs are a workaround.
-    const routeSearchRef = "route_search" as ReferenceExpression<DB, "route_search">;
-    const routeSelectRef = ['rowid as id', 'name', 'sector_name', 'route_name'] as SelectExpression<DB, "route_search">[];
     const routes = await db.selectFrom('route_search')
-        .select(routeSelectRef)
-        .where(routeSearchRef, sql`MATCH`, `${query}`)
+        .where(sql`route_search`, 'match', query)
+        .select([sql<number>`rowid`.as('id'), 'name', 'sector_name', 'crag_name'])
         .limit(10)
         .execute();
 
