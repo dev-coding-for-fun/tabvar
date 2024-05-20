9d1182eb1637baece54efdd106a4640e7bfc70b9
dev-coding-for-fun <dserink@gmail.com>
2024-05-20 12:56:46 -0600

    updated migration for FTS table

---
+++ migrations/0011_add_columns_to_FTS.sql
@@ -0,0 +1,40 @@
+-- Migration number: 0011 	 2024-05-20T18:37:11.752Z
+ALTER TABLE "route"
+    ADD "pitch_count" INTEGER;
+
+DROP TABLE IF EXISTS "route_search";
+
+CREATE VIRTUAL TABLE "route_search" USING fts5(
+    "name",
+    sector_name,
+    crag_name,
+    grade_yds UNINDEXED,
+    bolt_count UNINDEXED,
+    pitch_count UNINDEXED,
+    content='route',
+    content_rowid='id');
+
+
+DROP TRIGGER IF EXISTS route_search_insert;
+CREATE TRIGGER route_search_insert AFTER INSERT ON "route" BEGIN
+    INSERT INTO route_search(rowid, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count) 
+    VALUES (new.id, new.name, new.sector_name, new.crag_name, new.grade_yds, new.bolt_count, new.pitch_count);
+END;
+
+DROP TRIGGER IF EXISTS route_search_del;
+CREATE TRIGGER route_search_del AFTER DELETE ON "route" BEGIN
+    INSERT INTO route_search(route_search, rowid, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count) 
+    VALUES('delete', old.id, old.name, old.sector_name, old.crag_name, old.grade_yds, old.bolt_count, old.pitch_count);
+END;
+
+DROP TRIGGER IF EXISTS route_search_update;
+CREATE TRIGGER route_search_update AFTER UPDATE ON "route" BEGIN
+    INSERT INTO route_search(route_search, rowid, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count)
+    VALUES('delete', old.id, old.name, old.sector_name, old.crag_name, old.grade_yds, old.bolt_count, old.pitch_count);
+    INSERT INTO route_search(rowid, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count)
+    VALUES(new.id, new.name, new.sector_name, new.crag_name, new.grade_yds, new.bolt_count, new.pitch_count);
+END;
+
+INSERT INTO route_search (rowid, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count) SELECT id, name, sector_name, crag_name, grade_yds, bolt_count, pitch_count FROM route;
+
+INSERT INTO route_search(route_search) VALUES('optimize');
