cbdae831c78643052beb64350e1ba771686abe98
dev-coding-for-fun <dserink@gmail.com>
2024-05-22 18:00:39 -0600

    add react plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [remixCloudflareDevProxy(), remix(), react(), tsconfigPaths()],
   resolve: {
     alias: {
       // Alias Node.js core modules to their prefixed versions
