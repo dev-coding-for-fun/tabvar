8b1dba6a81ff26a291fb3ebdc0465f3c7d8333b8
dev-coding-for-fun <dserink@gmail.com>
2024-05-23 21:14:13 -0600

    add console query logging

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -8,5 +8,11 @@ export function getDB(context: AppLoadContext) {
   const env = context.cloudflare.env as unknown as Env;
   return new Kysely<DB>({
     dialect: new D1Dialect({ database: env.DB }),
+    log(event) {
+      if (event.level === 'query') {
+        console.log(event.query.sql)
+        console.log(event.query.parameters)
+      }
+    }
   });
 }
