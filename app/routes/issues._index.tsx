ce7df092e2eef3234e96d898374703c2a45cd2e5
dev-coding-for-fun <dserink@gmail.com>
2024-06-09 14:19:20 -0600

    add no-data message and fix focus issue

--- app/routes/issues._index.tsx
+++ app/routes/issues._index.tsx
@@ -1,11 +1,11 @@
-import { Badge, Center, Container, Group, Select, Text } from "@mantine/core";
+import { Badge, Center, Container, Group, Select, Stack, Text, Title } from "@mantine/core";
 import { LoaderFunction, json } from "@remix-run/cloudflare";
-import { useFetcher, useLoaderData } from "@remix-run/react";
+import { Link, useFetcher, useLoaderData } from "@remix-run/react";
 import { Crag, Issue } from "kysely-codegen";
 import { DataTable } from "mantine-datatable";
 import { getDB } from "~/lib/db";
 import { IconClick } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const PAGE_SIZE = 15;
 
@@ -31,6 +31,8 @@ export default function IssuesIndex() {
     const crags = useLoaderData<Crag[]>();
     const fetcher = useFetcher();
     const [issues, setIssues] = useState<IssueWithRoute[]>([]);
+    const [selectedCrag, setSelectedCrag] = useState<{ id: string | null; name: string | null }>({ id: null, name: null });
+    const cragSelectRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (fetcher.data) {
@@ -40,59 +42,72 @@ export default function IssuesIndex() {
 
     return (
         <Container size="xl" p="md">
-            <Select
-                label="Pick a crag"
-                placeholder="Pick one"
-                searchable
-                maxDropdownHeight={600}
-                data={ crags.map((crag) => ({ value: crag.id.toString(), label: crag.name ?? '' }))}
-                onChange={(value) => {
-                    fetcher.load(`/api/issues?cragid=${value}`);
-                }}
-            />
-            <DataTable
-                withTableBorder
-                borderRadius="sm"
-                withColumnBorders
-                striped
-                highlightOnHover
-                records={issues}
-                columns={[
-                    {
-                        accessor: "id",
-                        textAlign: "right",
-                    },
-                    {
-                        accessor: "route_name",
-                        render: (record) =>
-                            <Group>
-                                <Text>{record.route_name}</Text>
-                                <Badge size="xs" color="sector-color">{record.sector_name}</Badge>
-                                <Badge size="xs" color="crag-color">{record.crag_name}</Badge>
-                            </Group>,
-                    },
-                    {
-                        accessor: "issue_type",
-                    },
-                    {
-                        accessor: "sub_issue_type",
-                    },
-                    {
-                        accessor: "description",
-                    },
-                    {
-                        accessor: "status",
-                        render: (record) =>
-                            <Badge size="md" color={`status-${record.status}`}>{record.status}</Badge>,
-                    },
-                    {
-                        accessor: "actions",
-                        title: (<Center><IconClick size={16} /></Center>),
-                        width: '0%',
-                    },
-                ]}
 
-            />
+            <Stack>
+                <Title order={1}>Route Issues</Title>
+                <Link to={`/issues/create`}>Submit new issue</Link>
+                <Select
+                    label="Pick a crag"
+                    placeholder="Pick one"
+                    searchable
+                    maxDropdownHeight={600}
+                    data={crags.map((crag) => ({ value: crag.id.toString(), label: crag.name ?? '' }))}
+                    onChange={(value) => {
+                        const selectedCrag = crags.find(crag => crag.id?.toString() === value);
+                        if (selectedCrag) {
+                            setSelectedCrag({ id: selectedCrag.id.toString(), name: selectedCrag.name });
+                            fetcher.load(`/api/issues?cragid=${value}`);
+                            cragSelectRef.current?.blur();
+                        }
+                    }}
+                    ref={cragSelectRef}
+                />
+                <DataTable
+                    withTableBorder
+                    borderRadius="sm"
+                    withColumnBorders
+                    striped
+                    highlightOnHover
+                    minHeight={150}
+                    noRecordsText={ selectedCrag.id ? `No issues found at ${selectedCrag.name}.` : "Select a crag to search for issues." }
+                    records={issues}
+                    columns={[
+                        {
+                            accessor: "id",
+                            textAlign: "right",
+                        },
+                        {
+                            accessor: "route_name",
+                            render: (record) =>
+                                <Group>
+                                    <Text>{record.route_name}</Text>
+                                    <Badge size="xs" color="sector-color">{record.sector_name}</Badge>
+                                    <Badge size="xs" color="crag-color">{record.crag_name}</Badge>
+                                </Group>,
+                        },
+                        {
+                            accessor: "issue_type",
+                        },
+                        {
+                            accessor: "sub_issue_type",
+                        },
+                        {
+                            accessor: "description",
+                        },
+                        {
+                            accessor: "status",
+                            render: (record) =>
+                                <Badge size="md" color={`status-${record.status}`}>{record.status}</Badge>,
+                        },
+                        {
+                            accessor: "actions",
+                            title: (<Center><IconClick size={16} /></Center>),
+                            width: '0%',
+                        },
+                    ]}
+
+                />
+            </Stack>
         </Container>
     );
 }
