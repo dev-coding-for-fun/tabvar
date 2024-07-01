7682b5d7fb2d103d5b2084770c8a2c6b24de87ac
dev-coding-for-fun <dserink@gmail.com>
2024-07-01 11:39:06 -0600

    warn on unused instead of error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -74,6 +74,9 @@ module.exports = {
         "plugin:import/recommended",
         "plugin:import/typescript",
       ],
+      rules: {
+        "@typescript-eslint/no-unused-vars": "warn"
+      }
     },
 
     // Node
