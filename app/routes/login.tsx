e7b50d7dd77693624aeccdde979418df7e027212
dev-coding-for-fun <dserink@gmail.com>
2024-07-01 21:26:11 -0600

    pretty up login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,9 +1,34 @@
+import { Title, Paper, Stack, Center, Text, Image, Button, Container } from "@mantine/core";
 import { Form } from "@remix-run/react";
 
 export default function Login() {
-    return (
-      <Form action="/auth/google" method="post">
-        <button>Login with Google</button>
-      </Form>
-    )
-  }
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center" fw={900}>
+        TABVAR Login
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        Sign in to access route and issue management
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <Form action="/auth/google" method="post">
+          <Stack>
+            <Center>
+            <Button
+                leftSection={<Image src="/google.png" alt="Google logo" width={30} height={30} />}
+                variant="default"
+                color="gray"
+                type="submit"
+                fullWidth
+                style={{ maxWidth: '240px' }}
+              >
+                Continue with Google
+              </Button>
+            </Center>
+          </Stack>
+        </Form>
+      </Paper>
+    </Container>
+  );
+}
