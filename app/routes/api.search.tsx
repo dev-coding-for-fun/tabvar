bd8343be7460395b4eb4b7df2f81731641ddf1c1
dev-coding-for-fun <dserink@gmail.com>
2025-02-17 18:24:13 -0700

    add modified search for crags/sectors

--- app/routes/api.search.tsx
+++ app/routes/api.search.tsx
@@ -5,10 +5,12 @@ import { RouteSearch } from 'kysely-codegen';
 
 export interface RouteSearchResults extends RouteSearch {
     id: number;
+    type: string;
 }
 
 export const loader: LoaderFunction = async ({ request, context }) => {
     const url = new URL(request.url);
+    const separateRouteCragSector = url.searchParams.get('separateRouteCragSector') === 'true';
     const query = url.searchParams.get('query')?.replace(/[^a-zA-Z0-9\s]/g, '') || null;
     if (!query) {
         return new Response(JSON.stringify({ routes: [] }), {
@@ -18,20 +20,68 @@ export const loader: LoaderFunction = async ({ request, context }) => {
     }
     console.log(query);
     const db = getDB(context);
-    const routes = await db.selectFrom('route_search')
-        .where(sql`route_search`, 'match', `"${query}" *`)
-        .select([sql<number>`rowid`.as('id'),
-            'name',
-            'sector_name',
-            'crag_name',
-            'grade_yds',
-            'bolt_count',
-            'pitch_count'
-        ])
-        .limit(10)
-        .execute();
 
-    const response: RouteSearchResults[] = routes;
-
-    return json(response);
+    if (separateRouteCragSector) {
+        const results: RouteSearchResults[] = await db.selectFrom('route_search')
+            .distinct()
+            .where(sql`route_search`, 'match', `crag_name:${query}*`)
+            .select([
+                sql<number>`NULL`.as('id'),
+                sql<string>`'crag'`.as('type'),
+                sql<string>`''`.as('name'),
+                sql<string>`NULL`.as('sector_name'),
+                'crag_name',
+                sql<string>`NULL`.as('grade_yds'),
+                sql<string>`NULL`.as('bolt_count'),
+                sql<string>`NULL`.as('pitch_count')
+            ])
+            .unionAll(
+                db.selectFrom('route_search')
+                    .distinct()
+                    .where(sql`route_search`, 'match', `sector_name:${query}*`)
+                    .select([
+                        sql<number>`NULL`.as('id'),
+                        sql<string>`'sector'`.as('type'),
+                        sql<string>`''`.as('name'),
+                        sql<string>`sector_name`.as('sector_name'),
+                        sql<string>`crag_name`.as('crag_name'),
+                        sql<string>`NULL`.as('grade_yds'),
+                        sql<string>`NULL`.as('bolt_count'),
+                        sql<string>`NULL`.as('pitch_count')
+                    ])) 
+            .unionAll(
+                db.selectFrom('route_search')
+                    .where(sql`route_search`, 'match', `name:${query}*`)
+                    .select([
+                        sql<number>`rowid`.as('id'),
+                        sql<string>`'route'`.as('type'),
+                        sql<string>`name`.as('name'),
+                        sql<string>`sector_name`.as('sector_name'),
+                        sql<string>`crag_name`.as('crag_name'),
+                        sql<string>`grade_yds`.as('grade_yds'),
+                        sql<string>`bolt_count`.as('bolt_count'),
+                        sql<string>`pitch_count`.as('pitch_count')
+                    ])
+            ) 
+            .limit(10)
+            .execute();
+        console.log(results);
+        return json(results);
+    }
+    else {
+        const routes: RouteSearchResults[] = await db.selectFrom('route_search')
+            .where(sql`route_search`, 'match', `"${query}" *`)
+            .select([sql<number>`rowid`.as('id'),
+                'name',
+                sql<string>`'route'`.as('type'),
+                'sector_name',
+                'crag_name',
+                'grade_yds',
+                'bolt_count',
+                'pitch_count'
+            ])
+            .limit(10)
+            .execute();
+        return json(routes);
+    }
 }
