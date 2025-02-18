a0b66360cf9faff44f3aeeabe6f683a9e605696b
dev-coding-for-fun <dserink@gmail.com>
2025-02-17 18:23:26 -0700

    add yosemite decimal grades constant

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,5 +1,7 @@
 export const PERMISSION_ERROR = "You do not have the required permissions to access this page.";
 
+export const YDS_GRADES = ['5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', '5.10a', '5.10b', '5.10c', '5.10d', '5.11a', '5.11b', '5.11c', '5.11d', '5.12a', '5.12b', '5.12c', '5.12d', '5.13a', '5.13b', '5.13c', '5.13d', '5.14a', '5.14b', '5.14c', '5.14d', '5.15a'];
+
 export function getLabelFromValue<T extends { value: string, label: string }[]>(items: T, valueToFind: string): string {
     const foundItem = items.find(item => item.value === valueToFind);
     return foundItem ? foundItem.label : valueToFind;
@@ -44,3 +46,4 @@ export const userRoles = [
     { value: 'admin', label: 'Admin' },
     { value: 'member', label: 'Member' },
 ] as const;
+
