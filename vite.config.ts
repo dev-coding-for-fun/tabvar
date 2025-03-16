39922ce2327e1912657951505d741ca5ddf7883e
dev-coding-for-fun <dserink@gmail.com>
2025-03-16 15:21:29 -0600

    add remix future flags

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,27 @@ import {
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 const config = defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [
+    remixCloudflareDevProxy(), 
+    remix({
+      future: {
+        unstable_optimizeDeps: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+      }
+    }), 
+    tsconfigPaths()
+  ],
   resolve: {
     alias: {
       // Alias Node.js core modules to their prefixed versions
