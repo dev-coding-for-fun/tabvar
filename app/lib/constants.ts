b9f210ba226d844b6393352a82a32f3dc890e2b1
dev-coding-for-fun <dserink@gmail.com>
2025-04-13 12:36:20 -0600

    merge ice and mixed ice

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,9 +1,8 @@
 export const PERMISSION_ERROR = "You do not have the required permissions to access this page.";
 
 export const YDS_GRADES = ['5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', '5.10a', '5.10b', '5.10c', '5.10d', '5.11a', '5.11b', '5.11c', '5.11d', '5.12a', '5.12b', '5.12c', '5.12d', '5.13a', '5.13b', '5.13c', '5.13d', '5.14a', '5.14b', '5.14c', '5.14d', '5.15a', '5.15b'];
-export const ICE_GRADES = ['WI1', 'WI2', 'WI3', 'WI4', 'WI5', 'WI6', 'WI7'];
+export const ICE_GRADES = ['WI1', 'WI2', 'WI3', 'WI4', 'WI5', 'WI6', 'WI7', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'M10', 'M11', 'M12', 'M13', 'M14', 'M15', 'M16'];
 export const BOULDER_GRADES = ['V0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16'];
-export const MIXEDICE_GRADES = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'M10', 'M11', 'M12', 'M13', 'M14', 'M15', 'M16'];
 export const AID_GRADES = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6'];
 export const CLIMB_STYLES = ['Sport', 'Trad', 'Boulder', 'Mixed Rock', 'Mixed Ice', 'Ice', 'Aid'];
 
@@ -15,13 +14,11 @@ export function getGradesbyStyle(style: string): string[] {
             return YDS_GRADES;
         case 'Boulder':
             return BOULDER_GRADES;
-        case 'Mixed Ice':
-            return MIXEDICE_GRADES;
         case 'Mixed Rock':
             return YDS_GRADES;
         case 'Trad':
             return YDS_GRADES;
-        case 'Ice':
+        case 'Ice/Mixed':
             return ICE_GRADES;
         case 'Aid':
             return AID_GRADES;
